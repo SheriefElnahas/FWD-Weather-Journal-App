8e0cba17e49a054a24211ffaf817f16ddc05b725
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-08-24 13:57:04 +0200

    project folder structure

---
+++ server.js
@@ -0,0 +1,26 @@
+const projectData = [];
+
+const express = require('express');
+
+const app = express();
+
+/* Dependencies - Bodyparser is used to parse our data */
+const bodyParser = require('body-parser');
+
+/* Middleware */
+// Here we are configuring express to use boder-parser as middle-ware
+// We use use method to tell body parser exatctly how we want our data to be dealt with - and we want JSON data 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+// Cors for cross origin allowance - cors is a package that let the browser and server talk to each other without any security interpetion
+const cors = require('cors');
+app.use(cors());
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+const server = app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
---
+++ website/app.js
@@ -0,0 +1,3 @@
+console.log('App File!');
+
+const apiKey = 'c75cc7fd1e1e7a7f403a0c4539068680&units=imperial';
